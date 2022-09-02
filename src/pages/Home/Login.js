091576aa6e5fa38ed85ba0314ae44534d9c2e90e
majowyyy <marjoriemarcelino011101@gmail.com>
2022-09-02 19:30:35 +0800

    updated login ui

--- src/pages/Home/Login.js
+++ src/pages/Home/Login.js
@@ -1,42 +1,50 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./Login.scss";
 
 const Login = () => {
-    
   return (
-    <div className="Auth-form-container">
-    <form className="Auth-form">
-      <div className="Auth-form-content">
-        <h3 className="Auth-form-title">Sign In</h3>
-        <div className="form-group mt-3">
-          <label>Username</label>
-          <input
-            type="text"
-            className="form-control mt-1"
-            placeholder="Enter username"
-          />
-        </div>
-        <div className="form-group mt-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control mt-1"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="d-grid gap-2 mt-3">
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right mt-2">
-        <a href="#">Forgot password?</a>
-        </p>
+    <div
+      className="background"
+      style={{ backgroundImage: 'url("images/bg.png")' }}
+    >
+      <div className="Auth-form-container">
+        <form className="Auth-form">
+          <div className="Auth-form-content">
+            <div class="center">
+              <img src="/images/logo.png" className="loginLogo" />
+            </div>
+            <div className="form-group mt-3">
+              <label>Username</label>
+              <input
+                type="text"
+                className="form-control mt-1"
+                placeholder="Enter username"
+              />
+            </div>
+            <div className="form-group mt-3">
+              <label>Password</label>
+              <input
+                type="password"
+                className="form-control mt-1"
+                placeholder="Enter password"
+              />
+            </div>
+            <div className="d-grid mt-1">
+              <p className="forgot-password text-right mt-1">
+                <a href="#">Forgot password?</a>
+              </p>
+            </div>
+            <div className="login-btn">
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
+    </div>
   );
-}
+};
 
 export default Login;
