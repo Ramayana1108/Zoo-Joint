46bd4c4658d8498668e66e4964796eb58d12b841
majowyyy <marjoriemarcelino011101@gmail.com>
2022-10-05 13:14:31 +0800

    updated

--- src/pages/Admin/Users/newUsers.js
+++ src/pages/Admin/Users/newUsers.js
@@ -262,7 +262,7 @@ const NewUser = () => {
               <button onClick={conditionalStatements} type="submit" className="btn btn-primary-add">
                 Register
               </button>
-              <button onClick={(e)=>{e.preventDefault(); if(window.confirm("Cancel?")){Cancel()}}} className="btn btn-primary-cancel">
+              <button onClick={(e)=>{e.preventDefault(); if(window.confirm("Are you sure you want to cancel?")){Cancel()}}} className="btn btn-primary-cancel">
                 Cancel
               </button>
             </div>
