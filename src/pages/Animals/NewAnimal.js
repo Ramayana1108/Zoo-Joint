3bae93faa40b719e916e241c9cba53f007e6eb64
Ramayana1108 <98857464+Ramayana1108@users.noreply.github.com>
2022-10-27 21:00:28 +0800

    upoadted

--- src/pages/Animals/NewAnimal.js
+++ src/pages/Animals/NewAnimal.js
@@ -233,6 +233,7 @@ const NewAnimal = () => {
                       }
                     );
                   }else{
+                    uploadTask2.cancel()
                     alert("Animal Added");
                     navigate("/animals");
                   }
